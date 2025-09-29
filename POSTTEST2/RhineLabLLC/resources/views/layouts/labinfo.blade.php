34f945d4933705c151fb33bfdedf48422bb04874
Vort32 <ariandas719@gmail.com>
2025-09-29 13:42:43 +0800

    tadi kehapus folder POSTTEST2

---
+++ POSTTEST2/RhineLabLLC/resources/views/layouts/labinfo.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title')</title>
+
+    @vite('resources/css/app.css')
+</head>
+<body class="antialiased bg-[#ECE8E5] min-h-screen relative overflow-hidden">
+    @yield('content')
+
+    <!-- Alpine.js -->
+    <script src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
+</body>
+</html>
